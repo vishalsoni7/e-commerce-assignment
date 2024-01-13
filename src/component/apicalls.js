6452d854a15e0d158606ffa40123faf2d9b72e67
vishal soni <vishsoni043@gmail.com>
2024-01-13 22:17:45 +0530

    refactor - fixed backend

--- src/component/apicalls.js
+++ src/component/apicalls.js
@@ -8,13 +8,13 @@ import {
   registerErrorToast,
 } from "./toast";
 
+const baseURL =
+  "https://2a4c31e6-154e-4cd2-929b-c02f67222e6c-00-1ja4cwlzj3j4f.pike.replit.dev";
+
 // Function to handle user login
 export const login = async (ecDispatch, userDetails, navigate) => {
   try {
-    const response = await axios.post(
-      "https://tripadvisor.vishalsoni7.repl.co/destinations/signin",
-      userDetails
-    );
+    const response = await axios.post(`${baseURL}/login`, userDetails);
     ecDispatch({ type: "LOGIN", payload: response.data });
     ecDispatch({ type: "LOGGED", payload: true });
     loginToast();
@@ -28,10 +28,7 @@ export const login = async (ecDispatch, userDetails, navigate) => {
 // Function to handle user registration
 export const resgister = async (ecDispatch, userDetails, navigate) => {
   try {
-    const response = await axios.post(
-      "https://tripadvisor.vishalsoni7.repl.co/destinations/signup",
-      userDetails
-    );
+    const response = await axios.post(`${baseURL}/register`, userDetails);
     ecDispatch({ type: "LOGIN", payload: response.data });
     ecDispatch({ type: "LOGGED", payload: true });
     registerToast(response.data);
