969bd2f1e839c64de9e46217c82d163feb9e171c
vishal soni <vishsoni043@gmail.com>
2024-01-14 19:36:09 +0530

    refactor

---
+++ src/data.js 19-34-45-667.js
@@ -0,0 +1,188 @@
+export const categoryData = [
+  {
+    id: 1,
+    category: "Sneakers",
+    img: [
+      "https://images.unsplash.com/photo-1552346154-21d32810aba3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80",
+
+      "https://images.unsplash.com/photo-1518002171953-a080ee817e1f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80",
+    ],
+  },
+  {
+    id: 2,
+    category: "Formals",
+    img: [
+      "https://images.unsplash.com/photo-1563434649554-58f91d22ec2c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2958&q=80",
+
+      "https://images.unsplash.com/photo-1543201017-a54835bbbc5d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80",
+    ],
+  },
+  {
+    id: 3,
+    category: "Runnings",
+    img: [
+      "https://images.unsplash.com/photo-1539185441755-769473a23570?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2942&q=80",
+
+      "https://images.unsplash.com/photo-1558004282-e2b2587e3e47?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2825&q=80",
+    ],
+  },
+];
+
+export const productsData = [
+  {
+    id: 1,
+    image:
+      "https://images.unsplash.com/photo-1533867617858-e7b97e060509?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2938&q=80",
+    ideal: "Men",
+    name: "Mens Formal Boots",
+    price: 39.99,
+    shoeSize: [7, 8, 9],
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, asperiores facere? Molestias impedit fuga placeat dolor  architecto! Cum impedit eum asperiores, eaque ipsum magni aperiam saepe officiis laborum? Delectus, accusantium.",
+  },
+  {
+    id: 2,
+    image:
+      "https://images.unsplash.com/photo-1449505278894-297fdb3edbc1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto.format&fit=crop&w=2940&q=80",
+    ideal: "Men",
+    name: "Formal Dark Tan Shoes",
+    price: 49.99,
+    shoeSize: [5, 6, 7],
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, asperiores facere? Molestias impedit fuga placeat dolor  architecto! Cum impedit eum asperiores, eaque ipsum magni aperiam saepe officiis laborum? Delectus, accusantium.",
+  },
+  {
+    id: 3,
+    image:
+      "https://images.unsplash.com/photo-1563434649554-58f91d22ec2c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2958&q=80",
+    ideal: "Men",
+    name: "Light Camel Leather Shoe",
+    price: 29.99,
+    shoeSize: [6, 7, 8],
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, asperiores facere? Molestias impedit fuga placeat dolor  architecto! Cum impedit eum asperiores, eaque ipsum magni aperiam saepe officiis laborum? Delectus, accusantium.",
+  },
+  {
+    id: 4,
+    image:
+      "https://images.unsplash.com/photo-1576133385309-203e67da8e58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto.format&fit=crop&w=2940&q=80",
+    ideal: "Men",
+    name: "Leather Boot",
+    price: 79.99,
+    shoeSize: [5, 6, 7, 8],
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, asperiores facere? Molestias impedit fuga placeat dolor  architecto! Cum impedit eum asperiores, eaque ipsum magni aperiam saepe officiis laborum? Delectus, accusantium.",
+  },
+  {
+    id: 5,
+    image:
+      "https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto.format&fit=crop&w=2940&q=80",
+    ideal: "Men",
+    name: "Nike Air Mid",
+    price: 89.99,
+    shoeSize: [7, 8, 9, 10],
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, asperiores facere? Molestias impedit fuga placeat dolor  architecto! Cum impedit eum asperiores, eaque ipsum magni aperiam saepe officiis laborum? Delectus, accusantium.",
+  },
+  {
+    id: 6,
+    image:
+      "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto.format&fit=crop&w=2940&q=80",
+    ideal: "Women",
+    name: "Nike Fitness Shoe",
+    price: 34.99,
+    shoeSize: [5, 6, 7],
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, asperiores facere? Molestias impedit fuga placeat dolor  architecto! Cum impedit eum asperiores, eaque ipsum magni aperiam saepe officiis laborum? Delectus, accusantium.",
+  },
+  {
+    id: 7,
+    image:
+      "https://images.unsplash.com/photo-1524532787116-e70228437bbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto.format&fit=crop&w=2938&q=80",
+    ideal: "Men",
+    name: "Nike Air Jordans",
+    price: 19.99,
+    shoeSize: [8, 9, 10],
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, asperiores facere? Molestias impedit fuga placeat dolor  architecto! Cum impedit eum asperiores, eaque ipsum magni aperiam saepe officiis laborum? Delectus, accusantium.",
+  },
+  {
+    id: 8,
+    image:
+      "https://images.unsplash.com/photo-1615290643080-e379930bf0cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto.format&fit=crop&w=2864&q=80",
+    ideal: "Women",
+    name: "Nike Low Dunk",
+    price: 59.99,
+    shoeSize: [6, 7, 8],
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, asperiores facere? Molestias impedit fuga placeat dolor  architecto! Cum impedit eum asperiores, eaque ipsum magni aperiam saepe officiis laborum? Delectus, accusantium.",
+  },
+  {
+    id: 9,
+    image:
+      "https://images.unsplash.com/photo-1608667508764-33cf0726b13a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto.format&fit=crop&w=2960&q=80",
+    ideal: "Men",
+    name: "Sneakers Puma",
+    price: 79.99,
+    shoeSize: [7, 8, 9],
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, asperiores facere? Molestias impedit fuga placeat dolor  architecto! Cum impedit eum asperiores, eaque ipsum magni aperiam saepe officiis laborum? Delectus, accusantium.",
+  },
+  {
+    id: 10,
+    image:
+      "https://images.unsplash.com/photo-1582588678413-dbf45f4823e9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto.format&fit=crop&w=2865&q=80",
+    ideal: "Women",
+    name: "Running Shoe",
+    price: 129.99,
+    shoeSize: [5, 6, 7, 8],
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, asperiores facere? Molestias impedit fuga placeat dolor  architecto! Cum impedit eum asperiores, eaque ipsum magni aperiam saepe officiis laborum? Delectus, accusantium.",
+  },
+  {
+    id: 11,
+    image:
+      "https://images.unsplash.com/photo-1605408499391-6368c628ef42?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto.format&fit=crop&w=1920&q=80",
+    ideal: "Men",
+    name: "Gaming Sneakers",
+    price: 79.99,
+    shoeSize: [8, 9, 10],
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, asperiores facere? Molestias impedit fuga placeat dolor  architecto! Cum impedit eum asperiores, eaque ipsum magni aperiam saepe officiis laborum? Delectus, accusantium.",
+  },
+  {
+    id: 12,
+    image:
+      "https://images.unsplash.com/photo-1590673846749-e2fb8f655df8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto.format&fit=crop&w=2940&q=80",
+    ideal: "Women",
+    name: "Winter White Shoe",
+    price: 129.99,
+    shoeSize: [5, 6, 7, 8],
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero, asperiores facere? Molestias impedit fuga placeat dolor  architecto! Cum impedit eum asperiores, eaque ipsum magni aperiam saepe officiis laborum? Delectus, accusantium.",
+  },
+];
+
+export const coroselImages = [
+  {
+    url: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1518656306295-aa28b28b2504?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2960&q=80",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1524532787116-e70228437bbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2938&q=80",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1552346154-21d32810aba3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1552066344-2464c1135c32?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1600185365778-7875a359b924?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2825&q=80",
+  },
+];
